badf7b18efd35f172507be93523ce50e3d859f4e
Utkarsh Mishra <iiutkarshmishraii@gmail.com>
2022-02-01 09:29:12 +0530

    initial react app

---
+++ enrollment_app/src/ui/App.tsx
@@ -0,0 +1,35 @@
+import { createTheme } from "@mui/material";
+import { blue } from "@mui/material/colors";
+import { Box, ThemeProvider } from "@mui/system";
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../redux/app/store";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+  },
+  components: {
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          borderRadius: "1rem",
+        },
+      },
+    },
+  },
+});
+export default function App() {
+  return (
+    <Provider store={store}>
+
+      <ThemeProvider theme={theme}>
+        <Box display="flex" minWidth="100vh" justifyContent="center">
+          <h2>Initial School Enrollment Application</h2>
+        </Box>
+      </ThemeProvider>
+    </Provider>
+  );
+}
